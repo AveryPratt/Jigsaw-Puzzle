ed92c8b4c2ddc5fe5e138b2bda14c07f2f00a2dd
Avery Pratt <apratt91@gmail.com>
2016-10-10 11:06:30 -0700

    granted access to images in js

--- app.js
+++ app.js
@@ -10,18 +10,19 @@ var canvas = document.getElementById('canvas');
 var startGameButton = document.getElementById('start-button');
 var scoreBoard = document.getElementById('scoreboard-table');
 var clearBoard = document.getElementById('clear-button');
-var pieces = [][];
+var pieces = [];
 
 for (var i = 0; i < x; i++) {
+  pieces[i] = [];
   for (var j = 0; j < y; j++) {
-    pieces[i][j] = Piece('./img/logo-' + x + '-' + y);
+    pieces[i][j] = new Piece('./img/easy/logo-' + i + '-' + j);
   }
 }
 
-function Piece(source){
-  this.x;
-  this.y;
-  this.randx;
-  this.randy;
+function Piece(source, randx, randy){
+  this.x = source.slice(16, 17);
+  this.y = source.slice(18, 19);
+  this.randx = randx;
+  this.randy = randy;
   this.imgSource = source;
 }
