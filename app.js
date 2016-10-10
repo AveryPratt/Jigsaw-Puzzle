f8b01ec710cebc3dde207542decc24e75016bd8c
Avery Pratt <apratt91@gmail.com>
2016-10-10 15:13:35 -0700

    finally working

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 'use strict';
 
 var timer = 0;
-var playerName = document.getElementById('player-name');
 // var dimensions = document.getElementById('dimensions');
 var x = 2;
 var y = 2;
 
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
-var startGameButton = document.getElementById('start-button');
+var canvasEl = document.getElementById('canvas');
+var ctx = canvasEl.getContext('2d');
+var startGameButtonEl = document.getElementById('start-button');
 var pieces = [];
 
 function Piece(source){
@@ -16,6 +15,7 @@ function Piece(source){
   // this.img.setAttribute('src', source);
   this.img = new Image(100, 100);
   this.img.src = source;
+  this.img.setAttribute('style', 'border: 1px solid #000000;');
   this.x = source.slice(14, 15);
   this.y = source.slice(16, 17);
   // this.randx = randx;
@@ -24,24 +24,26 @@ function Piece(source){
 
 function populatePieces(){
   for (var i = 0; i < x; i++) {
-    var newArr = [];
-    pieces.push(newArr);
+    pieces.push([]);
     for (var j = 0; j < y; j++) {
-      pieces[i][j] = new Piece('img/easy/logo-' + i + '-' + j + '.png');
+      pieces[i][j] = new Piece('img/easy/logo-' + j + '-' + i + '.png');
     }
   }
 }
 
 function drawCanvas(){
-  console.log(pieces);
-  console.log(pieces[0][0].img.src);
-  console.log(pieces[0][0].img);
-  ctx.drawImage(pieces[0][0].img, 0, 0, canvas.width, canvas.height);
+  for (var i = 0; i < pieces.length; i++) {
+    for (var j = 0; j < pieces[i].length; j++) {
+      ctx.drawImage(pieces[i][j].img, i * (canvas.width / x), j * (canvas.height / y), canvas.width / x, canvas.height / y);
+    }
+  }
+  console.log('pieces: ', pieces);
 }
 
 function startButtonClick() {
+  var playerNameInputEl = document.getElementById('player-name');
   populatePieces();
   drawCanvas();
 }
 
-startGameButton.addEventListener('click', startButtonClick);
+startGameButtonEl.addEventListener('click', startButtonClick);
