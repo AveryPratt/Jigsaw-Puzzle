ea56891b198b3db7fb0c52dfcd16191f4172061d
Avery Pratt <apratt91@gmail.com>
2016-10-10 16:43:51 -0700

    almost works

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var timer = 0;
+var myLocation;
 // var dimensions = document.getElementById('dimensions');
 var x = 2;
 var y = 2;
@@ -10,7 +11,7 @@ var ctx = canvasEl.getContext('2d');
 var startGameButtonEl = document.getElementById('start-button');
 var pieces = [];
 
-function Piece(source){
+function Piece(source, ind){
   // this.img = document.createElement('img');
   // this.img.setAttribute('src', source);
   this.img = new Image(100, 100);
@@ -18,17 +19,53 @@ function Piece(source){
   this.img.setAttribute('style', 'border: 1px solid #000000;');
   this.x = source.slice(14, 15);
   this.y = source.slice(16, 17);
+  this.ind = ind;
   // this.randx = randx;
   // this.randy = randy;
 };
 
 function populatePieces(){
+  var locations = [];
   for (var i = 0; i < x; i++) {
     pieces.push([]);
     for (var j = 0; j < y; j++) {
-      pieces[i][j] = new Piece('img/easy/logo-' + j + '-' + i + '.png');
+      myLocation = checkLocation(locations);
+      pieces[i][j] = new Piece('img/easy/logo-' + loc.yy + '-' + loc.xx + '.png');
+      locations.push(myLocation);
     }
   }
+}
+
+var loc;
+
+function checkLocation(locations){
+  generateRandLocation(0, x, 0, y);
+  var passed = true;
+  for (var i = 0; i < locations.length; i++) {
+    if(loc === locations[i]) {
+      passed = false;
+      console.log(passed, '= PASSED');
+    }
+  }
+  if(passed === false){
+    return checkLocation(locations);
+    console.log(checkLocation(locations), 'checkLocation(locations)');
+  } else {
+    return loc;
+  }
+}
+
+function ArrayLocation(yy, xx){
+  this.yy = yy;
+  this.xx = xx;
+}
+
+function generateRandLocation(minX, maxX, minY, maxY){
+  var rand1 = Math.round(Math.random(minX, maxX));
+  var rand2 = Math.round(Math.random(minY, maxY));
+  console.log(rand1, 'rand 1');
+  console.log(rand2, 'rand 2');
+  loc = new ArrayLocation(rand1, rand2);
 }
 
 function drawCanvas(){
